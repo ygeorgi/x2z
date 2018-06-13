cb356716563c3ae58cdb5c1e8cd8b3771f756904
avcopan <avcopan@gmail.com>
2018-06-13 10:22:03 -0500

    Cleans up the python bindings and adds helper functions for constructing objects. (#9)

--- src/x2ztools.cc
+++ src/x2ztools.cc
@@ -1,15 +1,56 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include "libx2z/atom.hh"
 
+namespace py = pybind11;
 
-int isotope(const std::string& s) {
-    AtomBase a = AtomBase(s);
-    return a.isotope();
+
+// Helpers
+AtomBase _atom_base(const std::string& l, int iso = 0) {
+    AtomBase a;
+    if (iso == 0)
+        a = AtomBase(l);
+    else
+        a = AtomBase(l, iso);
+    return a;
+}
+
+
+Atom _atom(const std::string& l, const std::vector<double>& xyz,
+                 int iso = 0) {
+    Atom a = Atom(l);
+    a[0] = xyz[0];
+    a[1] = xyz[1];
+    a[2] = xyz[2];
+    return a;
+}
+
+
+// Functions for python binding
+double mass(const std::string& l, int iso = 0) {
+    AtomBase a = _atom_base(l, iso);
+    return a.mass();
+}
+
+
+std::string atom_string(const std::string& l, const std::vector<double>& xyz,
+                        int iso = 0) {
+    Atom a = _atom(l, xyz, iso);
+    std::ostringstream stream;
+    stream << a;
+    std::string s = stream.str();
+    return s;
 }
 
 
 PYBIND11_MODULE(x2ztools, module) {
-    module.def("isotope", &isotope);
+    module.def("mass", &mass,
+               "Get the mass of an isotope",
+               py::arg("l"), py::arg("iso")=0);
+    module.def("atom_string", &atom_string,
+               "Get the string representation of an atom",
+               py::arg("l"), py::arg("xyz"), py::arg("iso")=0);
 }
--- test_x2ztools.py
+++ test_x2ztools.py
@@ -1,5 +1,12 @@
 import x2ztools
+import numpy
 
 
-def test__isotope():
-    assert x2ztools.isotope('C') == 12
+def test__mass():
+    assert numpy.allclose(x2ztools.mass('C'), 21874.661832)
+    assert numpy.allclose(x2ztools.mass('C', iso=13), 23703.6661089)
+
+
+def test__atom_string():
+    assert (x2ztools.atom_string('C', (1.123098123, 0., 2.1239081)) ==
+            ' C         1.1231              0        2.12391')
